395d3e0241a02dd03236cabb61fe71a87ece3406
Manikandan N <99782666+Starlord48@users.noreply.github.com>
2024-03-14 12:32:41 +0530

    Update Student.java

--- Student_record/src/userDefinedCollections/Student.java
+++ Student_record/src/userDefinedCollections/Student.java
@@ -1,17 +1,21 @@
 package userDefinedCollections;
 
+
 public class Student {
+	//student class data
 	private int studentID;
 	private String studentName;
 	private int studentRoll;
 	private String studentCourse;
 
+	//student constructor
 	public Student(int ID, String name, int roll, String course) {
 		studentID = ID;
 		studentName = name;
 		studentRoll = roll;
 		studentCourse = course;
 	}
+	// getter methods
 	public int getStudentID() {
 		return studentID;
 	}
@@ -24,7 +28,8 @@ public class Student {
 	public String getStudentCourse() {
 		return studentCourse;
 	}
-		
+	
+	// setter methods	
 	public void setStudentID(int ID) {
 		studentID = ID;
 		
@@ -40,6 +45,8 @@ public class Student {
 	public void setStudentCourse(String course) {
 		studentCourse = course;
 	}
+
+	// method to print the student datatype
 	public void printStudent() {
 		System.out.println("Student id: " + studentID+"\tStudent name: "+studentName+"\n student Roll: "+studentRoll+ "\t course: "+studentCourse);
 	}
