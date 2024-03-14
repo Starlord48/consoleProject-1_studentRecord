2dbce5fdcdebbd2b40c65799e89ab28bd3383f94
Manikandan N <99782666+Starlord48@users.noreply.github.com>
2024-03-14 12:30:15 +0530

    Update DbConnection.java

--- Student_record/src/userDefinedCollections/DbConnection.java
+++ Student_record/src/userDefinedCollections/DbConnection.java
@@ -1,13 +1,14 @@
 package userDefinedCollections;
 import java.sql.*;
 
+//A class to facilitate database connection
 public class DbConnection {
 	static String url = "jdbc:mysql://localhost:3306/school";
 	static String username = "root";
 	static String password = "07@ug200One";
 	
 	public static Connection connectDatabase() throws SQLException, ClassNotFoundException {
-			Class.forName("com.mysql.cj.jdbc.Driver");
+		Class.forName("com.mysql.cj.jdbc.Driver");
 	        return DriverManager.getConnection(url, username, password);
 	}
 }
